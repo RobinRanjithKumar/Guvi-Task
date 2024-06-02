149133b8b25021ec003a59ce2e4ab9d767f1f403
RobinRanjithKumar <probin1843@gmail.com>
2024-06-02 18:30:12 +0530

    modified Read about.js file

--- Day1/Read about.js
+++ Day1/Read about.js
@@ -1,3 +1,5 @@
+
+//IpAddress
 const os = require('os');
 
 function getLocalIpAddress() {
@@ -11,5 +13,25 @@ function getLocalIpAddress() {
     }
     return '0.0.0.0';
 }
+//MAC address
 
 console.log("Local IP Address:", getLocalIpAddress());
+
+
+const macaddress = require('macaddress');
+
+macaddress.all((err, all) => {
+    if (err) {
+        console.error("Error retrieving MAC addresses:", err);
+        return;
+    }
+    console.log("MAC Addresses:", all);
+});
+
+macaddress.one((err, mac) => {
+    if (err) {
+        console.error("Error retrieving MAC address:", err);
+        return;
+    }
+    console.log("Default MAC Address:", mac);
+});

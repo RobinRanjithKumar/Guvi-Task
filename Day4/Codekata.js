2e16c6c9f7c9296d8ffc26a5435570772287c547
RobinRanjithKumar <probin1843@gmail.com>
2024-06-21 12:43:28 +0530

    modified Codekata.js file

--- Day4/Codekata.js
+++ Day4/Codekata.js
@@ -1,3 +1,16 @@
+/*Write a code to get the input in the given format and print the output in the given format
+
+Input Description:
+A single line contains a string.
+
+Output Description:
+Print the characters in a string separated by space.
+
+Sample Input :
+guvi
+Sample Output :
+g u v i */
+
 // Getting input via STDIN
 const readline = require("readline");
 

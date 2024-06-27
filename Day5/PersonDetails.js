fb71fb6e2f2f69327b93df2ddd8f314d365a4989
RobinRanjithKumar <probin1843@gmail.com>
2024-06-27 20:34:39 +0530

    create PersonDetails.js file

---
+++ Day5/PersonDetails.js
@@ -0,0 +1,34 @@
+class Person {
+    constructor(firstName, lastName, age, gender, address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+        this.address = address;
+    }
+
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+    getDetails() {
+        return `
+            Name: ${this.getFullName()}
+            Age: ${this.age}
+            Gender: ${this.gender}
+            Address: ${this.address}
+        `;
+    }
+}
+
+// Example usage:
+const person1 = new Person("Robin", "RanjithKumar", 24, "Male", "61/AB Pongimmal Street Rathinapuri coimbatore, Tamilnadu");
+console.log(person1.getFullName());  // Output: John Doe
+console.log(person1.getDetails());  
+/*
+Output:
+Name: John Doe
+Age: 30
+Gender: Male
+Address: 123 Main St, Anytown, USA
+*/
